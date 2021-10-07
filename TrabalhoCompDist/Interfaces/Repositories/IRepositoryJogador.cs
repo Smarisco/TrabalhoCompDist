0230327b57e42142fed7cd4fa73cd832809a8ae8
Smarisco <56485970+Smarisco@users.noreply.github.com>
2021-10-07 13:10:06 -0400

    Criar Service

---
+++ TrabalhoCompDist/Interfaces/Repositories/IRepositoryJogador.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TrabalhoCompDist.Arguments.Jogador;
+
+namespace TrabalhoCompDist.Interfaces.Repositories
+{
+    public interface IRepositoryJogador
+    {
+        AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request);
+        Guid AdicionarJogador(AdicionaJogadorRequest request);
+    }
+}
--- TrabalhoCompDist/Services/ServiceJogador.cs
+++ TrabalhoCompDist/Services/ServiceJogador.cs
@@ -1,19 +1,55 @@
 ﻿using System;
 using TrabalhoCompDist.Arguments.Jogador;
+using TrabalhoCompDist.Interfaces.Repositories;
 using TrabalhoCompDist.Interfaces.Services;
 
 namespace TrabalhoCompDist.Services
 {
     class ServiceJogador : IServiceJogador
     {
+        //inversão de controle
+        private readonly IRepositoryJogador _repositoryJogador;
+
+        public ServiceJogador(IRepositoryJogador repositoryJogador)
+        {
+            _repositoryJogador = repositoryJogador;
+        }
         public AdicionarJogadorResponse AdicionarJogador(AdicionaJogadorRequest request)
         {
-            throw new NotImplementedException();
+            Guid id = _repositoryJogador.AdicionarJogador(request);
+            return new AdicionarJogadorResponse() { Id = id, Mensagem = "Operação realizada com sucesso" };
         }
 
         public AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request)
         {
-            throw new NotImplementedException();
+            if (request == null)
+            {
+                throw new Exception("Autenticar jogador é obrigatório");
+            }
+            if (string.IsNullOrEmpty(request.Email))
+            {
+                throw new Exception("Informe um e-mail");
+            }
+            if (IsEmail(request.Email))
+            {
+                throw new Exception("Informe um e-mail");
+            }
+            if (string.IsNullOrEmpty(request.Senha))
+            {
+                throw new Exception("Informe uma senha");
+            }
+            if (request.Senha.Length<6)
+            {
+                throw new Exception("Digite uma senha com mais de 6 caracteres");
+            }
+           var response = _repositoryJogador.AutenticarJogador(request);
+            return response;
+
+        }
+
+        private bool IsEmail(string email)
+        {
+            return false;
         }
     }
 }
