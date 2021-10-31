dd8a158ca4f9c6847dd1d97fd948143ae37bd699
Smarisco <56485970+Smarisco@users.noreply.github.com>
2021-10-31 12:56:40 -0400

    inicio Infra

--- Infraestrutura/Persistencia/Contexto.cs
+++ Infraestrutura/Persistencia/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using TrabalhoCompDist.Entities;
 
 
@@ -12,6 +13,27 @@ namespace Infraestrutura.Persistencia
             Configuration.LazyLoadingEnabled = false;
 
         }
-        public IDbSet<Jogador> jogadores { get; set; }
+        public IDbSet<Jogador> Jogadores { get; set; }
+        public IDbSet<Plataforma> Plataformas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //setar com schema
+            //modelBuilder.HasDefaultSchema(" ");
+            //Remove43 a pluralização dos nomes na tabela
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //Remove exclusão em cascata
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //Setar para usar varchar ou invés de nvarchar
+            modelBuilder.Properties<string>().Configure(p=>p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+                      
+
+        }
     }
 }
