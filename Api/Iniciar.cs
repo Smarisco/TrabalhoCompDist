caa4a7365dd65559245c55dad008784fd38bfd75
Smarisco <56485970+Smarisco@users.noreply.github.com>
2021-11-02 12:43:34 -0400

    atualizando

---
+++ Api/Iniciar.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Web.Http;
+using Unity;
+using Unity.Lifetime;
+using InversaoDeControle.Unity;
+
+namespace Api
+{
+    public class Iniciar
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Swagger
+            //SwaggerConfig.Register(config);
+
+            // Configure Injeção de dependência
+            var container = new UnityContainer();
+            Dependencia(container);
+            config.Dependencia = new Resolver(container);
+
+            ConfigureWebApi(config);
+            ConfigureOAuth(app, container);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            // Remove o XML
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            //Compacta retorno de cada requisição realizada para api
+            config.MessageHandlers.Insert(0, new ServerCompressionHandler(new GZipCompressor(), new DeflateCompressor()));
+
+            // Modifica a identação
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Modifica a serialização
+            formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+
+            Register(config);
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            //add Uow action filter globally
+            //config.Filters.Add(new UnitOfWorkActionFilter());
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        public void ConfigureOAuth(IAppBuilder app, UnityContainer container)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(2),
+                Provider = new AuthorizationProvider(container)
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
